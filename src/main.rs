ab518d4aaf4c6db669894c3e7f90e7b158de85cd
AtLeastEddie8 <ee3de33@icloud.com>
2023-10-20 23:47:11 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    //guess in this context needs a data type otherwise error
+    let guess: i32 = "42".parse().expect("Not a number!");
+    println!("The guess is: {guess}");
+    let num1 = 5;
+    let num2 = 10;
+    //this will overflow bc num3 is unsigned
+    //let num3: u32 = num1 - num2;
+    let num3 = num1 - num2;
+    println!("num1 - num2 = {num3}");
+
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+}
